c1fdba8fb9d95470bd090290ad4780daf83ec5ee
S.Y <329362341+songyi2026001-dotcom@users.noreply.github.com>
2026-09-15 12:08:21 +0800

    Configure GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com'
+  site: 'https://songyi2026001-dotcom.github.io',
+  base: '/sy-digital-self'
 });
